a3af26a54fc2f16e4b6adf1dccc7fd4cf3d7bc54
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-04-26 08:33:02 +0530

    Initial commit

---
+++ src/arrayMethods/map.js
@@ -0,0 +1,26 @@
+// returns a new copy of the array
+
+/* 
+    1. Create an empty array mapArr.
+    2. Loop through array elements.
+    3. Call function mapFunc with the current element as the argument.
+    4. Push the result of the mapFunc function to the mapArr array.
+    5. Return the mapArr array after going through all the elements.
+*/
+
+// map takes a callback.
+
+Array.prototype.customMap = function (callback) {
+  let resultArr = [];
+  const { length } = this;
+  console.log(this);
+
+  for (let i = 0; i < length; i++) {
+    let result = callback(this[i], i, this);
+    resultArr.push(result);
+  }
+
+  return resultArr;
+};
+
+console.log([1, 2, 3, 4, 5].customMap((item, index) => item * 10));
